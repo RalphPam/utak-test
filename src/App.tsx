3a66d5bdf4ba6bfc20cecb35b6a4d19882f4a727
RalphPam <pralphnest@gmail.com>
2022-06-27 09:56:32 +0800

    Create Layout for the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import 'antd/dist/antd.min.css'
+import { Layout } from 'antd'
+import styled from 'styled-components'
+
+const { Header, Content, Sider } = Layout
+
+const StyledLayout = styled(Layout)`
+  height: calc(100vh - 64px);
+`
+
 function App() {
-  return <div>Hello</div>
+  return (
+    <Layout>
+      <Header>Header</Header>
+      <StyledLayout>
+        <Content>Content</Content>
+        <Sider collapsedWidth={0}>Sider</Sider>
+      </StyledLayout>
+    </Layout>
+  )
 }
 
 export default App
