01ac6b02c14530feab6f76d8a1e3a8d24a5c81ca
RalphPam <pralphnest@gmail.com>
2022-06-27 15:46:38 +0800

    Sider > Change close icon and remove selected product after close

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -168,20 +168,12 @@ const ProductForm = ({
   return (
     <StyledSlider width={350} collapsedWidth={0} collapsed={formState === null}>
       <Header justify="space-between">
-        <Minimize onClick={() => setFormState(null)}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={30}
-            height={30}
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="#fff"
-            strokeWidth={2}>
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
+        <Minimize onClick={() => {
+          setFormState(null)
+          setSelectedProduct(null)
+        }}>
+          <svg xmlns="http://www.w3.org/2000/svg" width={30} height={30} fill="none" viewBox="0 0 24 24" stroke="#fff" strokeWidth={2}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
         </Minimize>
         <Space>
